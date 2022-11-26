338273e8fa84493316f324d0d9d3c0478b55cf38
Javier Tamez <tamezjavier@gmail.com>
2022-11-26 17:36:28 -0600

    Implement test for inheritance.

--- src/test/java/characters/TestDexter.java
+++ src/test/java/characters/TestDexter.java
@@ -1,7 +1,29 @@
 package characters;
 
+import org.junit.jupiter.api.*;
+import templates.IDog;
+import templates.StoryCharacter;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestDexter {
+    @Nested
+    @DisplayName("Check if Dexter implements IDog and StoryCharacter")
+    class CheckParents {
+        Dexter dexter;
+        @BeforeEach
+        void create_new_Dexter(){
+            dexter = new Dexter();
+        }
+        @Test
+        void should_implement_IDog() {
+            assertInstanceOf(IDog.class, dexter);
+        }
+
+        @Test
+        void should_inherit_StoryCharacter(){
+            assertInstanceOf(StoryCharacter.class, dexter);
+        }
+    }
 
 }
