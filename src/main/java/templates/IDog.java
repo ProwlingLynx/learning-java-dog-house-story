f6e748e418425fb07d6a984ada8879ed67f65a00
Javier Tamez <tamezjavier@gmail.com>
2022-11-26 17:57:58 -0600

    remove TestIDog. Add comment explaining reasoning behind decision.

--- src/main/java/templates/IDog.java
+++ src/main/java/templates/IDog.java
@@ -1,5 +1,7 @@
 package templates;
 
 public interface IDog {
+//    Note: Trying to test an interface directly feels like a fool's errand. There isn't any code to test :/
+//    I think it makes sense to instead make unit tests against a concrete class that implements the interface(s).
     String speak();
 }
--- src/test/java/templates/TestIDog.java
+++
@@ -1,7 +0,0 @@
-package templates;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class TestIDog {
-
-}
