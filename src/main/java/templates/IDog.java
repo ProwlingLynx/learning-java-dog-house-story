250fd4756fca1522c7de4f326bd4fde12d7d165c
Javier Tamez <tamezjavier@gmail.com>
2022-12-01 22:33:16 -0600

    Refactor methods from string output to void.

--- src/main/java/templates/IDog.java
+++ src/main/java/templates/IDog.java
@@ -3,9 +3,9 @@ package templates;
 public interface IDog {
 //    Note: Trying to test an interface directly feels like a fool's errand. There isn't any code to test :/
 //    I think it makes sense to instead make unit tests against a concrete class that implements the interface(s).
-    String speak();
-    String beg();
-    String wagTail();
-    String eat();
-    String chew();
+    void speak();
+    void beg();
+    void wagTail();
+    void eat();
+    void chew();
 }
