fc81d268aca040703966ce1fe5c06eee8bc64a8a
Javier Tamez <tamezjavier@gmail.com>
2022-12-01 22:32:49 -0600

    Add new story lines. Invoke dexter.speak.

--- src/main/java/teller/Story.java
+++ src/main/java/teller/Story.java
@@ -1,7 +1,12 @@
 package teller;
 
+import characters.Dexter;
+
 public class Story {
     public static void main(String[] args) {
         System.out.println("Our story starts with an adorable husky. His name is Dexter.");
+        System.out.println("Here is Dexter right now.");
+        Dexter dexter = new Dexter();
+        dexter.speak();
     }
 }
