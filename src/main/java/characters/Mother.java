1cbe76d4440b5a25f989619812df793f622dc2a1
Javier Tamez <tamezjavier@gmail.com>
2022-12-01 22:38:09 -0600

    Add Mother class

---
+++ src/main/java/characters/Mother.java
@@ -0,0 +1,9 @@
+package characters;
+
+import templates.StoryCharacter;
+
+public class Mother extends StoryCharacter {
+    public Mother(){
+        super("Wanda", "Mother of two children and Dexter");
+    }
+}
